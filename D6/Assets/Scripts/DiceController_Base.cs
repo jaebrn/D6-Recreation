3ece26cbb412560d750a76469a591faa979ddcc7
Jennifer Brown <brown292@sheridancollege.ca>
2021-04-15 11:49:11 -0400

    Dice color randomization
    Color stored as an int variable, generated randomly and displayed visually

--- D6/Assets/Scripts/DiceController_Base.cs
+++ D6/Assets/Scripts/DiceController_Base.cs
@@ -1,27 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DiceController_Base : MonoBehaviour
 {
    // die value management
-    [Range(0, 5)] // range is 0-5 for the purposes of calling the correct sprite from the array: a 0 value die displays a face value of 1
     [SerializeField]
-    protected int m_dieValue;
+    protected int m_dieValue, m_dieColor;
 
     // die face sprite array
     public Sprite[] dieFace;
     public SpriteRenderer spriteRend;
 
+    // die color array
+    public Color[] dieColor;
+
     // Start is called before the first frame update
     void Start()
     {
-        m_dieValue = Random.Range(0, 5);
+        // color and value values determined randomly within array range
+        m_dieValue = Random.Range(0, dieFace.Length);
+        m_dieColor = Random.Range(0, dieColor.Length);
+
+        // values applied and updated visually
+        spriteRend.sprite = dieFace[m_dieValue]; // sprite displayed corresponds to the die value 
+        spriteRend.color = dieColor[m_dieColor]; // sprite color updates according to color value
     }
 
     // Update is called once per frame
     void Update()
     {
-        spriteRend.sprite = dieFace[m_dieValue]; // sprite displayed corresponds to the die value 
+        
     }
 }
