403985cda9c20b1f8ec54f6d3d2bf4ecd2e0e6f1
Jennifer Brown <brown292@sheridancollege.ca>
2021-04-15 10:11:12 -0400

    Die value randomization
    Randomly selects an int from 0 - 5 for the die value on Start(); sprite updates accordingly

--- D6/Assets/Scripts/DiceController_Base.cs
+++ D6/Assets/Scripts/DiceController_Base.cs
@@ -16,7 +16,7 @@ public class DiceController_Base : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        m_dieValue = Random.Range(0, 5);
     }
 
     // Update is called once per frame
