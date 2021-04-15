1cabeee5955504536ae311f18644fcc9f18f3832
Jennifer Brown <brown292@sheridancollege.ca>
2021-04-15 10:04:44 -0400

    Singular die prefab which displayed sprites from array based on value
    Removed the other prefabs and just converted it to the singular one

---
+++ D6/Assets/Scripts/DiceController_Base.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceController_Base : MonoBehaviour
+{
+   // die value management
+    [Range(0, 5)] // range is 0-5 for the purposes of calling the correct sprite from the array: a 0 value die displays a face value of 1
+    [SerializeField]
+    protected int m_dieValue;
+
+    // die face sprite array
+    public Sprite[] dieFace;
+    public SpriteRenderer spriteRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRend.sprite = dieFace[m_dieValue]; // sprite displayed corresponds to the die value 
+    }
+}
