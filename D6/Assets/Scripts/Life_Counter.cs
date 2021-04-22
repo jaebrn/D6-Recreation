41007293a9d47ea0cc609475d888aeb1d69ef458
Jennifer Brown <brown292@sheridancollege.ca>
2021-04-21 20:17:00 -0400

    Life counter UI and sprite

---
+++ D6/Assets/Scripts/Life_Counter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life_Counter : MonoBehaviour
+{
+    public Sprite[] lifeCounter;
+    public SpriteRenderer spriteRend;
+    public int currentLives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLives = 6;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRend.sprite = lifeCounter[currentLives];
+    }
+
+    public void LoseLife()
+    {
+        currentLives = currentLives - 1;
+        if(currentLives <=0)
+        {
+            Debug.Log("Game Over");
+        }
+        else
+        {
+            spriteRend.sprite = lifeCounter[currentLives];
+            Debug.Log("Lose life");
+        }
+    }
+}
